d199e90204293f3458eb22fc84e666903af39118
j <staniojakub@gmail.com>
2026-03-27 01:57:17 +0300

    update icon and logo of the app

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -4,6 +4,7 @@
 	import 'geist-svelte/font/mono';
 
 	import favicon from '$lib/assets/favicon.svg';
+	import Logo from '$lib/components/logo.svelte';
 
 	let { children } = $props();
 </script>
@@ -13,8 +14,11 @@
 </svelte:head>
 
 <main class="mx-auto flex max-w-310 flex-col gap-4 p-4 py-2">
-	<header class="flex justify-start rounded-xl">
-		<img src={favicon} alt="logo" class="size-20" />
+	<header class="">
+		<div class="group flex cursor-pointer items-center justify-start">
+			<Logo class="h-15 w-20 transition-transform group-hover:scale-110" />
+			<h2 class="text-2xl">pxicons</h2>
+		</div>
 	</header>
 
 	{@render children()}
